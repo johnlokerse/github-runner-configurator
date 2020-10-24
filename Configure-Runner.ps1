da5c8f510377ec5bc1b6407dad987da0ad0594dc
John <J.G.Lokerse@gmail.com>
2020-10-24 18:02:45 +0200

    Moved the registration token

--- Configure-Runner.ps1
+++ Configure-Runner.ps1
@@ -6,9 +6,8 @@ if (-not $authToken -or $authToken -eq "Og==") {
     Write-Error -Message "'$authToken' is invalid"
 }
 
-[string] $registrationToken = (Get-ProjectRegistrationToken -Owner johnlokerse -Repo github-runner-configurator -Base64AuthToken $authToken).token
-
 Get-RunnerFiles
 if (Test-Path -Path $PWD\*.cmd) {
+    [string] $registrationToken = (Get-ProjectRegistrationToken -Owner johnlokerse -Repo github-runner-configurator -Base64AuthToken $authToken).token
     Install-Runner -GitHubRepoUrl https://github.com/johnlokerse/github-runner-configurator -RegistrationToken $registrationToken
 }
