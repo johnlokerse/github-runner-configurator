abc23fb9bfd29371a345aeccd1c706334ded63a5
John <J.G.Lokerse@gmail.com>
2020-10-24 18:02:09 +0200

    Created a module and made an initial configure script

--- Configure-Runner.ps1
+++ Configure-Runner.ps1
@@ -1,32 +1,14 @@
-function Get-RunnerFiles {
-    New-Item -ItemType Directory -Value actions-runner
-    Set-Location actions-runner
-    Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.273.4/actions-runner-win-x64-2.273.4.zip -OutFile actions-runner-win-x64-2.273.4.zip
-    Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.273.4.zip", "$PWD")
+Import-Module -Force .\GitHub.Runner.PSFunctions.psm1
+
+[string] $authToken = Get-GitHubAuthToken -GitHubUserName X -PAT Y
+
+if (-not $authToken -or $authToken -eq "Og==") {
+    Write-Error -Message "'$authToken' is invalid"
 }
 
-Function Get-GitHubAuthToken {
-    $token = "johnlokerse:-"
-    return [System.Convert]::ToBase64String([char[]]$token)
-}
+[string] $registrationToken = (Get-ProjectRegistrationToken -Owner johnlokerse -Repo github-runner-configurator -Base64AuthToken $authToken).token
 
-Function Get-RegistrationToken {
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $Owner,
-        [Parameter(Mandatory = $true)]
-        [string] $Repo
-    )
-
-
-    $info = @{
-        Uri     = "https://api.github.com/repos/$Owner/$Repo/actions/runners/registration-token"
-        Method  = "POST"
-        Headers = @{
-            Authorization = "Basic $(Get-GitHubAuthToken)"
-            Accept        = "application/vnd.github.v3+json"
-        }
-    }
-
-    Invoke-RestMethod @info
+Get-RunnerFiles
+if (Test-Path -Path $PWD\*.cmd) {
+    Install-Runner -GitHubRepoUrl https://github.com/johnlokerse/github-runner-configurator -RegistrationToken $registrationToken
 }
---
+++ GitHub.Runner.PSFunctions.psm1
@@ -0,0 +1,51 @@
+$baseUrl = "https://api.github.com"
+
+function Get-GitHubAuthToken {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $GitHubUserName,
+        [Parameter(Mandatory = $true)]
+        [string] $PAT
+    )
+
+    $token = "$GitHubUserName" + ":" + "$PAT"
+    return [System.Convert]::ToBase64String([char[]]$token)
+}
+
+function Get-ProjectRegistrationToken {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $Owner,
+        [Parameter(Mandatory = $true)]
+        [string] $Repo,
+        [Parameter(Mandatory = $true)]
+        [string] $Base64AuthToken
+    )
+
+    $restMethodBody = @{
+        Uri     = "$baseUrl/repos/$Owner/$Repo/actions/runners/registration-token"
+        Method  = "POST"
+        Headers = @{
+            Authorization = "Basic $Base64AuthToken"
+            Accept        = "application/vnd.github.v3+json"
+        }
+    }
+
+    Invoke-RestMethod @restMethodBody
+}
+
+function Get-RunnerFiles {
+    New-Item -ItemType Directory -Value actions-runner
+    Set-Location actions-runner
+    Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.273.4/actions-runner-win-x64-2.273.4.zip -OutFile actions-runner-win-x64-2.273.4.zip
+    Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.273.4.zip", "$PWD")
+}
+
+function Install-Runner {
+    [Parameter(Mandatory = $true)]
+    [string] $GitHubRepoUrl
+    [Parameter(Mandatory = $true)]
+    [string] $RegistrationToken
+    
+    Invoke-Item -Path $PWD\config.cmd --url $GitHubRepoUrl --token $RegistrationToken
+}
