db2474335494f560a9846840bea710bc54f9c447
John <J.G.Lokerse@gmail.com>
2020-10-25 16:24:40 +0100

    Added some Write-Host's

--- Configure-Runner.ps1
+++ Configure-Runner.ps1
@@ -4,17 +4,20 @@ param (
     [Parameter(Mandatory = $true)]
     [string] $GitHubUserName,
     [Parameter(Mandatory = $true)]
-    [string] $PAT
+    [string] $PAT,
+    [Parameter(Mandatory = $true)]
+    [string] $GitHubRepoUrl
 )
 
 [string] $authToken = Get-GitHubAuthToken -GitHubUserName $GitHubUserName -PAT $PAT
-
 if (-not $authToken -or $authToken -eq "Og==") {
     Write-Error -Message "'$authToken' is invalid" -ErrorAction Stop
 }
 
+Write-Host "Downloading GitHub Runner files..."
 Get-RunnerFiles
 if (Test-Path -Path $PWD\*.cmd) {
     [string] $registrationToken = (Get-ProjectRegistrationToken -Owner johnlokerse -Repo github-runner-configurator -Base64AuthToken $authToken).token
-    Install-Runner -GitHubRepoUrl https://github.com/johnlokerse/github-runner-configurator -RegistrationToken $registrationToken
+    Write-Host "Installing GitHub runner in $GitHubRepoUrl"
+    Install-Runner -GitHubRepoUrl $GitHubRepoUrl -RegistrationToken $registrationToken
 }
