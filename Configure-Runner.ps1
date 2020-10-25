b1288b537acd1ae0b777e58c10b61f0ecfb63dc5
John <J.G.Lokerse@gmail.com>
2020-10-25 16:38:20 +0100

    Removed hardcoded username and repo

--- Configure-Runner.ps1
+++ Configure-Runner.ps1
@@ -2,12 +2,22 @@ Import-Module -Force .\GitHub.Runner.PSFunctions.psm1
 
 param (
     [Parameter(Mandatory = $true)]
-    [string] $GitHubUserName,
+    [Alias("Username")][string] $GitHubUsername,
     [Parameter(Mandatory = $true)]
     [string] $PAT,
     [Parameter(Mandatory = $true)]
-    [string] $GitHubRepoUrl
+    [Alias("repo")][string] $GitHubRepoUrl
 )
+
+function Get-RepoName {
+    [array] $splittedUrl = ($GitHubRepoUrl -Split "/")
+    return $splittedUrl[$splittedUrl.Count - 1]
+}
+
+function Get-Owner {
+    [array] $splittedUrl = ($GitHubRepoUrl -Split "/")
+    return $splittedUrl[$splittedUrl.Count - 2]
+}
 
 [string] $authToken = Get-GitHubAuthToken -GitHubUserName $GitHubUserName -PAT $PAT
 if (-not $authToken -or $authToken -eq "Og==") {
@@ -17,7 +27,7 @@ if (-not $authToken -or $authToken -eq "Og==") {
 Write-Host "Downloading GitHub Runner files..."
 Get-RunnerFiles
 if (Test-Path -Path $PWD\*.cmd) {
-    [string] $registrationToken = (Get-ProjectRegistrationToken -Owner johnlokerse -Repo github-runner-configurator -Base64AuthToken $authToken).token
+    [string] $registrationToken = (Get-ProjectRegistrationToken -Owner Get-Owner -Repo Get-RepoName -Base64AuthToken $authToken).token
     Write-Host "Installing GitHub runner in $GitHubRepoUrl"
     Install-Runner -GitHubRepoUrl $GitHubRepoUrl -RegistrationToken $registrationToken
 }
