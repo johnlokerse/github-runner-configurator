c6087118ff8ad569695220ebb8332d9115b6554e
John <J.G.Lokerse@gmail.com>
2020-11-06 23:50:49 +0100

    Added Install-RunnerAsService

--- GitHub.Runner.PSFunctions.psm1
+++ GitHub.Runner.PSFunctions.psm1
@@ -44,10 +44,87 @@ function Get-RunnerFiles {
 function Install-Runner {
     param (
         [Parameter(Mandatory = $true)]
-        [string] $GitHubRepoUrl,
+        [Alias("Url")][string] $GitHubRepoUrl,
         [Parameter(Mandatory = $true)]
-        [string] $RegistrationToken
+        [Alias("Token")][string] $RegistrationToken,
+        [Parameter(Mandatory = $false)]
+        [string] $Name,
+        [Parameter(Mandatory = $false)]
+        [string] $WorkDirLocation
+    )
+
+    if (-not $Name) {
+        $Name = Get-Hostname
+    }
+
+    if (-not $WorkDirLocation) {
+        $WorkDirLocation = Get-CurrentDirectory
+    }
+
+    if (!(Verify-Path -Path $WorkDirLocation)) {
+        throw [System.ArgumentException] "Invalid work directory location"
+    }
+    
+    .\config.cmd --unattended --url $GitHubRepoUrl --token $RegistrationToken --name $Name --work $WorkDirLocation
+}
+
+function Install-RunnerAsService {
+    param (
+        [Parameter(Mandatory = $true)]
+        [Alias("Url")][string] $GitHubRepoUrl,
+        [Parameter(Mandatory = $true)]
+        [Alias("Token")][string] $RegistrationToken,
+        [Parameter(Mandatory = $true)]
+        [string] $LogonAccount,
+        [Parameter(Mandatory = $true)]
+        [string] $LogonPassword,
+        [Parameter(Mandatory = $false)]
+        [string] $Name,
+        [Parameter(Mandatory = $false)]
+        [string] $WorkDirLocation
+    )
+
+    if (-not $Name) {
+        $Name = Get-Hostname
+    }
+
+    if (-not $WorkDirLocation) {
+        $WorkDirLocation = Get-CurrentDirectory
+    }
+
+    if (!(Verify-Path -Path $WorkDirLocation)) {
+        throw [System.ArgumentException] "Invalid work directory location"
+    }
+
+    .\config.cmd --unattended --url $GitHubRepoUrl --token $RegistrationToken --runasservice --windowslogonaccount $LogonAccount --windowslogonpassword $LogonPassword --name $Name --work $WorkDirLocation
+}
+
+function Remove-Runner {
+    throw [System.NotImplementedException] "Remove runner has not been implemented yet!"    
+}
+
+#region Helpers
+function Get-Hostname {
+    return [System.Net.Dns]::GetHostname()
+}
+
+function Verify-Path {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $Path
     )
     
-    .\config.cmd --unattended --url $GitHubRepoUrl --token $RegistrationToken
+    return Test-Path -Path $Path
 }
+
+function Get-CurrentDirectory {
+    return (Get-Location).Path
+}
+#endregion Helpers
+
+Export-ModuleMember Get-GitHubAuthToken
+Export-ModuleMember Get-ProjectRegistrationToken
+Export-ModuleMember Get-RunnerFiles
+Export-ModuleMember Install-Runner
+Export-ModuleMember Install-RunnerAsService
+Export-ModuleMember Remove-Runner
